ae55d200e52c6486a896f7a5baddf255c5e06601
Jedsadaporn05 <jedsadapornnew05@gmail.com>
2025-02-26 03:59:09 +0700

    Add project files.

---
+++ HW02/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using HW02.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace HW02.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IWebHostEnvironment _hostingEnvironment;
+
+		public HomeController(ILogger<HomeController> logger, IWebHostEnvironment hostingEnvironment)
+		{
+			_logger = logger;
+			_hostingEnvironment = hostingEnvironment;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		public IActionResult Regis(Regis abs)
+		{
+			if (ModelState.IsValid)
+			{
+				if (abs.Image != null)
+				{
+					string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "images");
+
+					if (!Directory.Exists(uploadsFolder))
+					{
+						Directory.CreateDirectory(uploadsFolder);
+					}
+
+					string uniqueFileName = Guid.NewGuid().ToString() + "_" + abs.Image.FileName;
+
+					string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+					using (var fileStream = new FileStream(filePath, FileMode.Create))
+					{
+						abs.Image.CopyTo(fileStream);
+					}
+
+					ViewBag.ImageUrl = $"/images/{uniqueFileName}";
+				}
+				return View("Success", abs);
+			}
+			return View(abs);
+		}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ HW02/Models/Regis.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+namespace HW02.Models
+{
+	public class Regis
+	{
+		[Key]
+		[Required(ErrorMessage = "Enter Email")]
+		public string? Email { get; set; }
+		[Required(ErrorMessage = "Enter Username")]
+		public string? Username { get; set; }
+		[Required(ErrorMessage = "Enter Password")]
+		public string? Password { get; set; }
+		[Compare("Password", ErrorMessage = "Password Don't Match")]
+		[Required(ErrorMessage = "Confirm Password")]
+        public string? ConfirmPassword { get; set; }
+		[Required(ErrorMessage = "Enter Phone Number")]
+		public string? Phone {  get; set; }
+		[Required(ErrorMessage = "Please Upload Image")]
+		[Display(Name = "Image")]
+		public IFormFile? Image { get; set; }
+		[Required(ErrorMessage = "Please Select Your Gender")]
+		[Display(Name = "Gender")]
+		public string? Gender { get; set; }
+	}
+}
